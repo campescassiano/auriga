74f1ab191a3efd75c7612e8ee2f1d58364f4d723
Cassiano Campes <cassianocampes@gmail.com>
2023-10-27 18:58:14 -0300

    improved build.sh file

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@
 set -eu
 set -o pipefail
 
-gcc -Wall main.c -o test_is
+output="test_is"
+
+gcc -Wall main.c -o $output
+
+if [[ "$#" -eq 0 ]]; then
+    echo "Build completed, output=$output"
+else
+    echo "Error during build"
+fi
